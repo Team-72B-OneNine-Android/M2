2ff3a7c44991962ab34e826d2ef351552ebf5c33
Gauranshu Sharma <gsharma@lawn-143-215-90-213.lawn.gatech.edu>
2019-01-23 00:34:35 -0500

    Person 4 completed

--- src/main/java/edu/gatech/oad/antlab/person/Person4.java
+++ src/main/java/edu/gatech/oad/antlab/person/Person4.java
@@ -9,49 +9,54 @@ package edu.gatech.oad.antlab.person;
  *  @version 1.1
  */
 public class Person4 {
-    /** Holds the persons real name */
-    private String name;
-    /**
-     * The constructor, takes in the persons
-     * name
-     * @param pname the person's real name
-     */
+
+  /** Holds the persons real name */
+  private String name;
+
+  /**
+   * The constructor, takes in the persons
+   * name
+   * @param pname the person's real name
+   */
   public Person4(String pname) {
-    name = pname;
+      name = pname;
   }
-    /**
-     * This method should return a string
-     * where each character is 1 greater
-     * than its previous value.  So
-     * given "abc123" it should return
-     * "bcd234".
-     *
-     * @param input the string to be modified
-     * @return the modified string
-     */
-    private String calc(String input) {
-      //Person 4 put your implementation here
-      // for (int i = 0, n = input.length(); i < n; i++) {
-      //   input[i] = str.charAt(i) + 1;
-      return input;
-    }
 
-    /**
-     * Return a string rep of this object
-     * that varies with an input string
-     *
-     * @param input the varying string
-     * @return the string representing the
-     *         object
-     */
-    public String toString(String input) {
-      return name + calc(input);
+/**
+ * This method should return a string
+ * where each character is 1 greater
+ * than its previous value.  So
+ * given "abc123" it should return
+ * "bcd234".
+ *
+ * @param input the string to be modified
+ * @return the modified string
+ */
+  private String calc(String input) {
+    //Person 4 put your implementation here
+    String incremented = new String();
+    for(int i=0; i < input.length(); i++){
+      incremented += (char)(input.charAt(i)+1);
     }
+    return incremented;
+  }
 
-    public static void main (String[] args) throws java.lang.Exception
-    {
-    Person4 test = new Person4("gauranshu")
-    }
+  /**
+   * Return a string rep of this object
+   * that varies with an input string
+   *
+   * @param input the varying string
+   * @return the string representing the
+   *         object
+   */
+  public String toString(String input) {
+    return name + calc(input);
+  }
+
+  public static void main(java.lang.String[] args) {
+      Person4 test = new Person4("gauranshu");
+      System.out.println(test.calc("gauranshu"));
+  }
 
 }
 
